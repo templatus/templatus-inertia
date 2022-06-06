801c3fc37feeb38643ee21b09253e8169f2271f4
Georg Ledermann <georg@ledermann.dev>
2022-06-06 05:57:14 +0200

    Inertia: Use commit_version

--- config/initializers/inertia_rails.rb
+++ config/initializers/inertia_rails.rb
@@ -1,20 +1,3 @@
-require 'digest'
-
 InertiaRails.configure do |config|
-  def checksum(dir)
-    files = Dir["#{dir}/**/*"].reject { |f| File.directory?(f) }
-    content = files.map { |f| File.read(f) }.join
-    Digest::MD5.hexdigest(content)
-  end
-
-  asset_path =
-    if Rails.env.test?
-      Rails.root.join('public', 'vite-test')
-    elsif Rails.env.production?
-      Rails.root.join('public', 'vite')
-    elsif Rails.env.development?
-      Rails.root.join('app', 'vite-dev')
-    end
-
-  config.version = checksum(asset_path)
+  config.version = Rails.configuration.x.git.commit_version
 end
