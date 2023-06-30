2b7559df2f88dc3f63ec3ed7c9d46c22e5cf5664
Georg Ledermann <georg@ledermann.dev>
2023-06-30 08:36:59 +0200

    Simplify test

--- spec/javascript/components/GitVersion.test.ts
+++ spec/javascript/components/GitVersion.test.ts
@@ -1,20 +1,15 @@
-import { waitFor, render } from '@testing-library/svelte';
+import { render } from '@testing-library/svelte';
 import '@testing-library/jest-dom';
 import GitVersion from '@/components/GitVersion.svelte';
 
 describe('GitVersion', () => {
   test('renders version', async () => {
     const { getByText } = render(GitVersion, {
-      target: document.body,
       props: {
         commitVersion: 'v0.0.1-123-7654321',
         commitTime: '2021-06-01T12:00:00+02:00',
       },
     });
-
-    await waitFor(() =>
-      expect(getByText('v0.0.1-123-7654321')).toBeInTheDocument(),
-    );
 
     expect(getByText('v0.0.1-123-7654321')).toBeInTheDocument();
   });
