29283e5ed74a0a9f033bc6ca7bed1107a2f4bff2
Georg Ledermann <georg@ledermann.dev>
2023-07-04 09:31:04 +0200

    Switch from jest to vitest for JavaScript testing

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [svelte({ hot: !process.env.VITEST })],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './app/javascript'),
+    },
+  },
+});
