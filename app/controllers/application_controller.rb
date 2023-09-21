8d75bc3a5554499493ca0e29be52f16411c8588b
Georg Ledermann <georg@ledermann.dev>
2023-09-21 09:15:33 +0200

    Reformat

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   include InertiaErrors
 
-  inertia_share flash: -> {
+  inertia_share flash: -> do
                   { notice: flash.notice, alert: flash.alert }.compact
-                }
+                end
 end
