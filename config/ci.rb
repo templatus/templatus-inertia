e5ebdd7015839446c192ddb70a6b9590518341ee
Georg Ledermann <georg@ledermann.dev>
2025-10-28 06:38:35 +0100

    Fix and update Local CI

--- config/ci.rb
+++ config/ci.rb
@@ -4,13 +4,15 @@ CI.run do
   step 'Setup', 'bin/setup --skip-server'
 
   step 'Style: Ruby', 'bin/rubocop'
+  step 'Style: JavaScript', 'bin/yarn lint'
+  step 'Style: TypeScript', 'bin/yarn tsc'
 
-  # step 'Security: Importmap vulnerability audit', 'bin/importmap audit'
+  step 'Security: Gem audit', 'bin/bundler-audit'
+  step 'Security: Yarn vulnerability audit', 'bin/yarn npm audit --recursive'
   step 'Security: Brakeman code analysis',
        'bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error'
 
-  step 'Tests: Rails', 'bin/rails test'
-  # step 'Tests: System', 'bin/rails test:system'
+  step 'Tests: Rails', 'bin/rspec'
   step 'Tests: Seeds', 'env RAILS_ENV=test bin/rails db:seed:replant'
 
   # Optional: set a green GitHub commit status to unblock PR merge.
