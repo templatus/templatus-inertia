369d115925420ec017c8b02df9da54a8018e30f7
Georg Ledermann <georg@ledermann.dev>
2026-01-08 06:23:54 +0100

    Fix warning

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,10 +38,10 @@ Rails.root.glob('spec/support/**/*.rb').each { |f| require f }
 # If you are not using ActiveRecord, you can remove these lines.
 begin
   ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
+rescue ActiveRecord::PendingMigrationError
   exit 1
 end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   # config.fixture_path = Rails.root.join('spec', 'fixtures')
